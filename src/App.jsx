6e4bbbacd6b2095d6b42ff73f166ff0bca3d3fe2
Gaille Amolong <gaille@bitworksolutions.com>
2026-08-17 17:00:30 +0800

    chore(init): Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <div className="App bg-cyan-900 text-white text-center text-9xl min-h-dvh">
+        Larp Larp Sahur
+    </div>
+  );
+}
+
+export default App;
